70db7be38a6ff83855877b350932c647943e5d0a
Johan <johan.aspenlind@b3.se>
2019-12-09 21:05:51 +0100

    Using electron-forge template with Webpack & React instead

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,25 @@
 import { app, BrowserWindow } from "electron";
-import * as path from "path";
+declare var MAIN_WINDOW_WEBPACK_ENTRY: any;
 
-let mainWindow: Electron.BrowserWindow;
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
 
-function createWindow() {
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow: any;
+
+const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    },
     width: 800,
+    height: 600
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../index.html"));
+  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
@@ -26,7 +31,7 @@ function createWindow() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
-}
+};
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -43,12 +48,12 @@ app.on("window-all-closed", () => {
 });
 
 app.on("activate", () => {
-  // On OS X it"s common to re-create a window in the app when the
+  // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (mainWindow === null) {
     createWindow();
   }
 });
 
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
