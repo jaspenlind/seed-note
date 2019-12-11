cd95d4eff709fe2f0d560c56ef1adf0a3de0754f
Johan <johan.aspenlind@b3.se>
2019-12-11 23:35:21 +0100

    feat: added material

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,64 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import { chords } from "tonal-index";
 import { orderBy } from "lodash";
+import { AppBar, Toolbar, Typography, InputBase, List, ListItem, ListItemText } from "@material-ui/core";
+import { createStyles, fade, Theme, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1,
+      display: "none",
+      [theme.breakpoints.up("sm")]: {
+        display: "block"
+      }
+    },
+    search: {
+      position: "relative",
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      "&:hover": {
+        backgroundColor: fade(theme.palette.common.white, 0.25)
+      },
+      marginLeft: 0,
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(1),
+        width: "auto"
+      }
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: "100%",
+      position: "absolute",
+      pointerEvents: "none",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center"
+    },
+    inputRoot: {
+      color: "inherit"
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+      transition: theme.transitions.create("width"),
+      width: "100%",
+      [theme.breakpoints.up("sm")]: {
+        width: 120,
+        "&:focus": {
+          width: 200
+        }
+      }
+    }
+  })
+);
 
 type ChordProps = {
   notes: string;
@@ -13,30 +71,52 @@ const Chord = ({ notes }: ChordProps) => {
 
   const ranked = orderBy(result, x => x.notes.length, "desc");
 
-  return ranked.length > 0 ? (
-    <div>
-      <p>Chords</p>
+  return (
+    <List>
       {ranked.map((value, index) => (
-        <div key={index}>
-          {value.root}
-          {value.type}({value.notes})
-        </div>
+        <ListItem key={index} button>
+          <ListItemText>
+            {value.root}
+            {value.type} ({value.notes})
+          </ListItemText>
+        </ListItem>
       ))}
-    </div>
-  ) : (
-    <div></div>
+    </List>
   );
 };
 
 const App = () => {
   const [notes, setNotes] = useState("");
+  const classes = useStyles();
 
   return (
     <div>
-      <h1>seed-note</h1>
-      <label>
-        <input type="text" placeholder="Find chord" onChange={e => setNotes(e.target.value)} />
-      </label>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="open drawer">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography className={classes.title} variant="h6" noWrap>
+              seed-note
+            </Typography>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Find chord…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput
+                }}
+                inputProps={{ "aria-label": "search" }}
+                onChange={e => setNotes(e.target.value)}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
       <Chord notes={notes}></Chord>
     </div>
   );
